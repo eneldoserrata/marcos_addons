81f485863bdaad1e9d0d73d3fed16fad37e8ae3a
Eneldo Serrata <eneldoserrata@gmail.com>
2016-02-14 00:18:56 -0400

    IMP

--- sale_control/models/models.py
+++ sale_control/models/models.py
@@ -3,18 +3,18 @@ from openerp import api, models, fields, SUPERUSER_ID
 
 
 class pos_config(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
-    allow_payments = fields.Boolean('Permitir cobrar', default=True)
-    allow_delete_order = fields.Boolean('Allow remove non-empty order', default=True)
-    allow_discount = fields.Float('Maximo descuento permitido en (%)', default=0)
-    allow_edit_price = fields.Boolean('Permitir cambiar el precio', default=True)
-    allow_delete_order = fields.Boolean('Permitir eliminar orde no vacías en el POS', default=True)
-    allow_refund = fields.Boolean("Permitir hacer devoluciones", default=False)
-    allow_delete_order_line = fields.Boolean('Permitir eliminar línea de la orden', default=True)
-    allow_cancel = fields.Boolean("Pueden cancelar ordenes nuevas")
-    allow_cash_refund = fields.Boolean("Permitir devolver dinero de la caja", default=False)
-    allow_credit = fields.Boolean("Permitir facturar a credito", default=False)
+    allow_payments = fields.Boolean(u"Permitir cobrar", default=True)
+    allow_delete_order = fields.Boolean(u"Allow remove non-empty order", default=True)
+    allow_discount = fields.Float(u"Maximo descuento permitido en (%)", default=0)
+    allow_edit_price = fields.Boolean(u"Permitir cambiar el precio", default=True)
+    allow_delete_order = fields.Boolean(u"Permitir eliminar orde no vacías en el POS", default=True)
+    allow_refund = fields.Boolean(u"Permitir hacer devoluciones", default=False)
+    allow_delete_order_line = fields.Boolean(u"Permitir eliminar línea de la orden", default=True)
+    allow_cancel = fields.Boolean(u"Pueden cancelar ordenes nuevas")
+    allow_cash_refund = fields.Boolean(u"Permitir devolver dinero de la caja", default=False)
+    allow_credit = fields.Boolean(u"Permitir facturar a credito", default=False)
 
 
 
