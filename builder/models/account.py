4f4ad22fb029e26a51be7b266daea2ec7a20db7d
Eneldo Serrata <eneldoserrata@gmail.com>
2016-05-24 13:32:40 -0400

    Add categ_ig group on account_move_line tree

--- builder/models/account.py
+++ builder/models/account.py
@@ -7,3 +7,9 @@ class account_analytic_line(models.Model):
     _inherit = 'account.analytic.line'
 
     categ_id = fields.Many2one(related="product_id.product_tmpl_id.categ_id", store=True)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move.line"
+
+    categ_id = fields.Many2one(related="product_id.product_tmpl_id.categ_id", store=True)
