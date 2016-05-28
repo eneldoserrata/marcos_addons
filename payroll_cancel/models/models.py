dde9a0ee7808268a9923977cd612d2a3ef84deec
Eneldo Serrata <eneldoserrata@gmail.com>
2016-05-28 03:59:35 -0400

    Add new module

---
+++ payroll_cancel/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, exceptions
+
+
+class HrPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    @api.multi
+    def delete_done_sheet(self):
+        for rec in self:
+            if not rec.move_id.journal_id.update_posted:
+                raise exceptions.UserError("Primero deberia Permitir cancelación de asientos para este diario.")
+            else:
+                rec.move_id.button_cancel()
+                rec.move_id.unlink()
+                rec.state = 'draft'
+                rec.unlink()
+
