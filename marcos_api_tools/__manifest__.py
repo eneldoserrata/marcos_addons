d581e74a27b1570f692f062a62bebd868e6a741d
eneldoserrata <eneldoserrata@gmail.com>
2017-03-07 01:24:32 -0400

    remove security warning

--- marcos_api_tools/__manifest__.py
+++ marcos_api_tools/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'data/data.xml',
         'views/views.xml',
         'views/templates.xml',
