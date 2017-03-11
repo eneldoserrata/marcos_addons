fd399909cd77a19ba22fa85781f28cfffee36bc9
eneldoserrata <eneldoserrata@gmail.com>
2017-03-10 22:58:10 -0400

    Testing sphinx project page on github

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndic":0,busqued:0,modul:0,pagin:0},titles:["Welcome to Marcos Addons&#8217;s documentation!"],titleterms:{addons:0,and:0,documentation:0,indic:0,marc:0,tabl:0,welcom:0}})
