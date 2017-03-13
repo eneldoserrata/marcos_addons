c26d93fb44d17713f08caa2a2d3c8c89a207c8de
eneldoserrata <eneldoserrata@gmail.com>
2017-03-13 05:07:22 -0400

    pep 8 format

--- l10n_do_bank_statement_import/models/bank_statement_import.py
+++ l10n_do_bank_statement_import/models/bank_statement_import.py
@@ -12,6 +12,7 @@ from odoo.addons.base.res.res_bank import sanitize_account_number
 
 from babel.dates import format_date
 
+
 class InheritedAccountBankStatementImport(models.TransientModel):
     _inherit = 'account.bank.statement.import'
 
@@ -37,9 +38,11 @@ class InheritedAccountBankStatementImport(models.TransientModel):
             vals_line = {
                 'date': transaction.date,
                 'name': transaction.payee + (transaction.memo and ': ' + transaction.memo or ''),
-                'ref': u"{}: {}".format(int(transaction.id)-1, format_date(transaction.date, 'EEEE d', locale=locale)),
+                'ref': u"{}: {}".format(int(transaction.id) - 1,
+                                        format_date(transaction.date, 'EEEE d', locale=locale)),
                 'amount': transaction.amount,
-                'unique_import_id': "{}-{}".format(transaction.id, transaction.date.strftime(DEFAULT_SERVER_DATE_FORMAT)),
+                'unique_import_id': "{}-{}".format(transaction.id,
+                                                   transaction.date.strftime(DEFAULT_SERVER_DATE_FORMAT)),
                 'bank_account_id': bank_account_id,
                 'partner_id': partner_id,
             }
@@ -50,14 +53,15 @@ class InheritedAccountBankStatementImport(models.TransientModel):
         min_date = min(dates)
         max_date = max(dates)
         vals_bank_statement = {
-            'name': "Del {} al {} de {}".format(min_date.strftime("%d"), max_date.strftime("%d"), format_date(max_date, 'MMMM', locale=locale)),
+            'name': "Del {} al {} de {}".format(min_date.strftime("%d"), max_date.strftime("%d"),
+                                                format_date(max_date, 'MMMM', locale=locale)),
             'transactions': transactions,
             'balance_start': float(ofx.account.statement.balance) - total_amt,
             'balance_end_real': ofx.account.statement.balance,
             'date': max_date.strftime(DEFAULT_SERVER_DATE_FORMAT)
         }
 
-        bank_journal_id = self.env["account.journal"].search([('bank_acc_number','=',ofx.account.number)])
+        bank_journal_id = self.env["account.journal"].search([('bank_acc_number', '=', ofx.account.number)])
         if not bank_journal_id:
             raise UserError(u"Debe espesificar el número de la cuenta del banco en el diario!")
 
@@ -67,7 +71,6 @@ class InheritedAccountBankStatementImport(models.TransientModel):
             currency = "DOP"
 
         return currency, ofx.account.number, [vals_bank_statement]
-
 
     def _find_additional_data(self, currency_code, account_number):
         """ Look for a res.currency and account.journal using values extracted from the
@@ -92,7 +95,8 @@ class InheritedAccountBankStatementImport(models.TransientModel):
                 journal.set_bank_account(account_number)
             # Already a bank account on the journal : check it's the same as on the statement
             elif journal and journal.bank_account_id.sanitized_acc_number != sanitized_account_number:
-                raise UserError(_('The account of this statement (%s) is not the same as the journal (%s).') % (account_number, journal.bank_account_id.acc_number))
+                raise UserError(_('The account of this statement (%s) is not the same as the journal (%s).') % (
+                account_number, journal.bank_account_id.acc_number))
             # No journal passed to the wizard : try to find one using the account number of the statement
             elif not journal:
                 journal = journal_obj.search([('bank_account_id.sanitized_acc_number', '=', sanitized_account_number)])
@@ -102,10 +106,10 @@ class InheritedAccountBankStatementImport(models.TransientModel):
             journal_currency = journal.currency_id
             if currency is None:
                 currency = journal_currency
-            # if currency and currency != journal_currency:
-            #     statement_cur_code = not currency and company_currency.name or currency.name
-            #     journal_cur_code = not journal_currency and company_currency.name or journal_currency.name
-            #     raise UserError(_('The currency of the bank statement (%s) is not the same as the currency of the journal (%s) !') % (statement_cur_code, journal_cur_code))
+                # if currency and currency != journal_currency:
+                #     statement_cur_code = not currency and company_currency.name or currency.name
+                #     journal_cur_code = not journal_currency and company_currency.name or journal_currency.name
+                #     raise UserError(_('The currency of the bank statement (%s) is not the same as the currency of the journal (%s) !') % (statement_cur_code, journal_cur_code))
 
         # If we couldn't find / can't create a journal, everything is lost
         if not journal and not account_number:
--- l10n_do_bank_statement_import/models/res_bank.py
+++ l10n_do_bank_statement_import/models/res_bank.py
@@ -6,4 +6,4 @@ from odoo import models, fields
 class InheritedBank(models.Model):
     _inherit = "res.bank"
 
-    statement_import_type = fields.Selection([('bpdofx','OFX')], string=u"Formato de importación de extractos")
+    statement_import_type = fields.Selection([('bpdofx', 'OFX')], string=u"Formato de importación de extractos")
