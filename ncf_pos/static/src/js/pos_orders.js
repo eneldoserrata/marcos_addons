0c54af3fb4899e95fd1df1dbbb867fc986865799
eneldoserrata <eneldoserrata@gmail.com>
2017-04-04 17:05:53 -0400

    IMP search by vat on pos

--- ncf_pos/static/src/js/pos_orders.js
+++ ncf_pos/static/src/js/pos_orders.js
@@ -9,7 +9,6 @@ odoo.define('ncf_pos.pos_orders', function (require) {
     var QWeb = core.qweb;
     var SuperPosModel = models.PosModel.prototype;
 
-    var PosDB = require('point_of_sale.DB');
 
 
     models.load_models([{
@@ -52,60 +51,6 @@ odoo.define('ncf_pos.pos_orders', function (require) {
         },
     },], {
         'after': 'product.product'
-    });
-
-    var DB = PosDB.include({
-        add_partners: function (partners) {
-            var updated_count = 0;
-            var new_write_date = '';
-            var partner;
-            for (var i = 0, len = partners.length; i < len; i++) {
-                partner = partners[i];
-
-                if (this.partner_write_date &&
-                    this.partner_by_id[partner.id] &&
-                    new Date(this.partner_write_date).getTime() + 1000 >=
-                    new Date(partner.write_date).getTime()) {
-                    // FIXME: The write_date is stored with milisec precision in the database
-                    // but the dates we get back are only precise to the second. This means when
-                    // you read partners modified strictly after time X, you get back partners that were
-                    // modified X - 1 sec ago.
-                    continue;
-                } else if (new_write_date < partner.write_date) {
-                    new_write_date = partner.write_date;
-                }
-                if (!this.partner_by_id[partner.id]) {
-                    this.partner_sorted.push(partner.id);
-                }
-                this.partner_by_id[partner.id] = partner;
-
-                updated_count += 1;
-            }
-
-            this.partner_write_date = new_write_date || this.partner_write_date;
-
-            if (updated_count) {
-                // If there were updates, we need to completely
-                // rebuild the search string and the barcode indexing
-
-                this.partner_search_string = "";
-                this.partner_by_barcode = {};
-
-                for (var id in this.partner_by_id) {
-                    partner = this.partner_by_id[id];
-                    console.log(partner.vat);
-                    if (partner.barcode) {
-                        this.partner_by_barcode[partner.barcode] = partner;
-                    }
-                    partner.address = (partner.street || '') + ', ' +
-                        (partner.vat || '') + ' ' +
-                        (partner.city || '') + ', ' +
-                        (partner.country_id[1] || '');
-                    this.partner_search_string += this._partner_search_string(partner);
-                }
-            }
-            return updated_count;
-        }
     });
 
     models.PosModel = models.PosModel.extend({
@@ -272,5 +217,5 @@ odoo.define('ncf_pos.pos_orders', function (require) {
             });
         }
     });
-    return OrdersScreenWidget, DB;
+    return OrdersScreenWidget;
 });
