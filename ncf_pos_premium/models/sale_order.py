fa69a33683a34cf480334cd16da510dffd98ccf4
Eneldo Serrata <eneldoserrata@gmail.com>
2017-11-07 02:15:30 -0400

    reverse unneeded change

--- ncf_pos_premium/models/sale_order.py
+++ ncf_pos_premium/models/sale_order.py
@@ -8,9 +8,6 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_confirm(self):
-        without_tax = self.partner_id.credit_limit_without_tax
-        net_receivable = ((self.partner_id.credit - self.partner_id.receivable_tax)
-                          if (self.partner_id.receivable_tax < self.partner_id.credit) else self.partner_id.credit)
         confirmed_orders_total = 0.00
         draft_invoices_total = 0.00
         confirmed_orders = self.search([('partner_id', '=', self.partner_id.id), ('state', 'in', ['sale', 'done']),
@@ -19,26 +16,23 @@ class SaleOrder(models.Model):
             [('partner_id', '=', self.partner_id.id), ('state', '=', 'draft')])
         if draft_invoices:
             draft_invoices_total = sum(map(
-                lambda x: x.amount_untaxed / x.currency_id.rate if without_tax else x.amount_total / x.currency_id.rate,
+                lambda x: x.amount_total / x.currency_id.rate,
                 draft_invoices))
         if confirmed_orders:
-            confirmed_orders_total = sum(map(lambda
-                                                 x: x.amount_untaxed / x.pricelist_id.currency_id.rate if without_tax else x.amount_total / x.pricelist_id.currency_id.rate,
-                                             confirmed_orders))
-        total_credit = ((
-                        self.amount_untaxed / self.pricelist_id.currency_id.rate if without_tax else self.amount_total / self.pricelist_id.currency_id.rate) +
-                        (
-                        net_receivable if without_tax else self.partner_id.credit) + confirmed_orders_total + draft_invoices_total)
+            confirmed_orders_total = sum(
+                map(lambda x: x.amount_total / x.pricelist_id.currency_id.rate, confirmed_orders))
+        total_credit = ((self.amount_total / self.pricelist_id.currency_id.rate) + (
+        self.partner_id.credit) + confirmed_orders_total + draft_invoices_total)
 
         if self.partner_id.credit_limit < total_credit:
             extra = total_credit - self.partner_id.credit_limit
-            view = self.env['ir.model.data'].xmlid_to_res_id('customers_credit_limit.climit_override_credit_form')
+            view = self.env['ir.model.data'].xmlid_to_res_id('ncf_pos_premium.climit_override_credit_form')
             ctx = self.env.context.copy()
             ctx.update({'order_id': self.id})
             return {
-                'name': _(
-                    u'Warning: Credit limit will be exceeded by {}{:,.2f}'.format(self.company_id.currency_id.symbol,
-                                                                                  extra)),
+                'name':
+                    u'Advertencia: El límite de crédito será excedido por {}{:,.2f}'.format(self.company_id.currency_id.symbol,
+                                                                                  extra),
                 'type': 'ir.actions.act_window',
                 'view_type': 'form',
                 'view_mode': 'form',
