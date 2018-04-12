2715a76c551ea893c8162fba6248e1e5ab946286
Eneldo Serrata <eneldoserrata@gmail.com>
2018-04-12 10:08:28 -0400

    fix fiscal sale labels

--- ncf_manager/models/account_invoice.py
+++ ncf_manager/models/account_invoice.py
@@ -120,11 +120,11 @@ class AccountInvoice(models.Model):
     ncf_control = fields.Boolean(related="journal_id.ncf_control")
     purchase_type = fields.Selection(related="journal_id.purchase_type")
     sale_fiscal_type = fields.Selection([
-        ("final", u"Consumidor final"),
-        ("fiscal", u"Para credito fiscal"),
+        ("final", u"Consumidor Final"),
+        ("fiscal", u"Crédito Fiscal"),
         ("gov", u"Gubernamental"),
-        ("special", u"Regimenes especiales"),
-        ("unico", u"Unico ingreso")
+        ("special", u"Regímenes Especiales"),
+        ("unico", u"Único Ingreso")
     ], string="NCF para", default="final")
     purchase_fiscal_type = fields.Selection([
         ('01', u'01 - Gastos de personal'),
